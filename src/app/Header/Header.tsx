3d96f4e39d8bf1d298b8a459b9608defad50bcee
Jonas Moreira <jonasmoreira@Jonass-MacBook-Pro.local>
2024-12-15 16:08:22 -0300

    Create LoginDialog component

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,52 +1,54 @@
-import React from 'react'
+import React from "react";
 import { Button } from "@/components/ui/button";
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
-  } from "@/components/ui/navigation-menu";
-  import { LogInIcon } from "lucide-react";
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { LogInIcon } from "lucide-react";
 
 const Header = () => {
   return (
     <header className="container mx-auto h-16 border-b border-slate-200 shadow-md flex items-center justify-between px-4">
+      <div>
+        <Link href="/" legacyBehavior passHref>
+          Blog educacional
+        </Link>
+      </div>
+
+      <nav className="flex items-center space-x-4">
         <div>
-          <Link href="/" legacyBehavior passHref>Blog educacional</Link>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Posts
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/sobre" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Sobre
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
 
-        <nav className="flex items-center space-x-4">
-          <div>
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                      Posts
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href="/sobre" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                      Sobre
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
+        <div>
+          <Button>
+            <LogInIcon /> Login
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-          <div>
-            <Button>
-              <LogInIcon /> Login
-            </Button>
-          </div>
-        </nav>
-      </header>
-  )
-}
-
-export default Header
+export default Header;
