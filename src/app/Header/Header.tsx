79131c3a7687519ed4fce2ab79abc2eacda3002d
Heloise Santos <heloise.hssantos@gmail.com>
2024-12-15 12:44:52 -0300

    ajustes código

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -12,40 +12,40 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="container mx-auto h-16 p-4 border-b border-slate-200 shadow-md flex items-center justify-between">
+    <header>
+      <div>
+        <Link href="/" legacyBehavior passHref>Blog educacional</Link>
+      </div>
+
+      <nav className="flex items-center space-x-4">
         <div>
-          <Link href="/" legacyBehavior passHref>Blog educacional</Link>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Posts
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/sobre" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Sobre
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
 
-        <nav className="flex items-center space-x-4">
-          <div>
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                      Posts
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href="/sobre" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                      Sobre
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
-
-          <div>
-            <Button>
-              <LogInIcon /> Login
-            </Button>
-          </div>
-        </nav>
-      </header>
+        <div>
+          <Button>
+            <LogInIcon /> Login
+          </Button>
+        </div>
+      </nav>
+    </header>
   )
 }
 
