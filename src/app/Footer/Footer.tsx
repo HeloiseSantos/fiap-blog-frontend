274fb9f584cd016d509bb9c3e77723a2d27ddc7f
Heloise Santos <heloise.hssantos@gmail.com>
2024-12-15 12:41:38 -0300

    ajustes layout footer

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <div className="container mx-auto p-4 text-center border border-slate-200">
-          <p>© 2024 - Feito por Heloíse Santos e Jonas Moreira - FIAP</p>
-        </div>
-      </footer>
+      <div className="container">
+        <p>© 2024 - Feito por Heloíse Santos e Jonas Moreira - FIAP</p>
+      </div>
+    </footer>
   )
 }
 
